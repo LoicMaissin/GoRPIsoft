a8375b57f5a17a2ef1334ed2cb1c25e27c4ffefd
Rémi Garde <remi.garde@free.fr>
2017-03-02 11:43:09 +0100

    Translate the actuator response into readable data

--- communication.go
+++ communication.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/hex"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -21,28 +22,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for count := 0; count < 100; count++ {
+	for count := 0; count < 1; count++ {
 		//	time.Sleep(time.Second / 15)
-		getAll(s)
+		x := actuatorInfo(getAll(s))
+		fmt.Print(x)
 	}
 }
 
-func getAll(s *serial.Port) {
+func getAll(s *serial.Port) []byte {
 	// Requête lire toutes les données
-	h, err := hex.DecodeString("011e001F00")
-	_, err = s.Write(h)
+	h, _ := hex.DecodeString("011e001F00")
+	_, err := s.Write(h)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Reads exactly 17 bytes
 	reader := bufio.NewReader(s)
-	reply, err := reader.Peek(32)
+	reply, err := reader.Peek(38)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Ouverture de la vanne (%)", reply[9])
-	log.Println("Couple subi (%)", reply[8])
-	writeDB(reply)
+	//	log.Println(reply)
+	//	log.Println("Couple subi (%)", reply[8])
+	return reply
 }
 
 func writeDB(d []byte) {
@@ -77,14 +79,93 @@ func writeDB(d []byte) {
 		"Opening":       d[9],
 	}
 
-	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
+	pt, err := client.NewPoint("Numerics", tags, fields, time.Now())
 	if err != nil {
 		log.Fatal(err)
 	}
 	bp.AddPoint(pt)
-
 	// Write the batch
 	if err := c.Write(bp); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func nthBit(x byte, n uint) bool {
+	if x>>n&1 == byte(0) {
+		return false
+	}
+	return true
+}
+
+func addBytes(v []byte) int {
+	// The number equal to v concatenated, with v[0] MSB
+	res := 0
+	for _, x := range v {
+		res = res<<4 + int(x)
+	}
+	return res
+}
+
+func actuatorInfo(response []byte) map[string]interface{} {
+	fields := map[string]interface{}{
+		"isOpened":                          nthBit(response[5], 0),
+		"isClosed":                          nthBit(response[5], 1),
+		"torqueLimiterActionOpenDirection":  nthBit(response[5], 2),
+		"torqueLimiterActionCloseDierction": nthBit(response[5], 3),
+		"selectorToLocalPosition":           nthBit(response[5], 4),
+		"selectorToRemotePosition":          nthBit(response[5], 5),
+		"selectorToOffPosition":             nthBit(response[5], 6),
+		"powerOn":                           nthBit(response[5], 7),
+
+		"actOpening":               nthBit(response[6], 0),
+		"actClosing":               nthBit(response[6], 1),
+		"handwheelAction":          nthBit(response[6], 2),
+		"ESDCommand":               nthBit(response[6], 3),
+		"actRunning":               nthBit(response[6], 4),
+		"actFault":                 nthBit(response[6], 5),
+		"positionSensorPowerFault": nthBit(response[6], 6),
+		"torqueSensorPowerFault":   nthBit(response[6], 7),
+
+		"lockedMotorOpen":      nthBit(response[7], 0),
+		"lockedMotorClose":     nthBit(response[7], 1),
+		"motorThermalOverload": nthBit(response[7], 2),
+		"lostPhase":            nthBit(response[7], 3),
+		"overtravelAlarm":      nthBit(response[7], 4),
+		"directionOpenAlarm":   nthBit(response[7], 5),
+		"directionCloseAlarm":  nthBit(response[7], 6),
+		"batteryLow":           nthBit(response[7], 7),
+
+		"runningTorque": response[8],
+		"actPosition":   response[9],
+
+		"indication1":       nthBit(response[10], 0),
+		"indication2":       nthBit(response[10], 1),
+		"indication3":       nthBit(response[10], 2),
+		"indication4":       nthBit(response[10], 3),
+		"indication5":       nthBit(response[10], 4),
+		"valveJammed":       nthBit(response[10], 5),
+		"Auxiliary24VFault": nthBit(response[10], 6),
+		"TooManyStarts":     nthBit(response[10], 7),
+
+		"pumping":              nthBit(response[11], 0),
+		"confMemFault":         nthBit(response[11], 1),
+		"activityMemFault":     nthBit(response[11], 2),
+		"baseMemFault":         nthBit(response[11], 3),
+		"stopMidTravel":        nthBit(response[11], 4),
+		"lostSignal":           nthBit(response[11], 5),
+		"partialStrokeRunning": nthBit(response[11], 6),
+		"partialStrokeFault":   nthBit(response[11], 7),
+
+		"openBreakoutMaxTorque": response[12],
+		"closeTightMaxTorque":   response[13],
+		"openingMaxTorque":      response[14],
+		"closingMaxTorque":      response[15],
+		"startsLast12h":         addBytes(response[16:18]),
+		"totalStarts":           addBytes(response[18:22]),
+		"totalRunningTime":      addBytes(response[22:26]),
+		"partialStarts":         addBytes(response[27:31]),
+		"partialRunningTime":    addBytes(response[31:35]),
+		"actPosition(per mil)":  addBytes(response[35:39]),
+	}
+	return fields
 }
